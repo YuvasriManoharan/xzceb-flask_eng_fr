c132cb9f29b2a752e7c172fa653a1b7f75da28ba
YuvasriManoharan <yuvamano17@gmail.com>
2023-07-22 11:39:54 -0400

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -1,26 +1,24 @@
 from machinetranslation import translator
-from flask import Flask, render_template, request
-import json
+from flask import Flask, render_template, request, jsonify
 
 app = Flask("Web Translator")
 
-@app.route("/english_to_french")
-def english_to_french():
-    textToTranslate = request.args.get('textToTranslate')
-    translated = translator.english_to_french(textToTranslate)
-    return "Translated text to French"
+@app.route('/englishToFrench')
+def translate_english_to_french():
+    text_to_translate = request.args.get('textToTranslate', '')
+    french_translation = translator.english_to_french(text_to_translate)
+    return jsonify({"translatedText": french_translation})
 
-@app.route("/french_to_english")
-def french_to_english():
-    textToTranslate = request.args.get('textToTranslate')
-    translated = translator.french_to_english(textToTranslate)
-    return "Translated text to English"
+@app.route('/frenchToEnglish')
+def translate_french_to_english():
+    text_to_translate = request.args.get('textToTranslate', '')
+    english_translation = translator.french_to_english(text_to_translate)
+    return jsonify({"translatedText": english_translation})
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
     return render_template("index.html")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
